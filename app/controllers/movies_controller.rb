76099a1f282aa0d89927cd78ccd8fb935b5dca83
chris-wamae <chriswamae123@gmail.com>
2023-03-16 09:49:56 +0300

    Completed Assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,5 +4,15 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies
   end
+  
+  def create 
+    new_movie = Movie.create(movie_params)
+    render json:new_movie, status: :created
+  end
 
+  private
+
+  def movie_params
+     params.permit(:title, :year, :length ,:director, :description, :poster_url, :category, :discount, :female_director)
+  end
 end
